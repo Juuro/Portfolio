608e9e6d78a6b7d2217f2cad0a0633e9843deabf
Juuro <mail@sebastian-engel.de>
2013-01-29 19:12:02 +0100

    Every category has its own URL.

--- js/script.js
+++ js/script.js
@@ -1,39 +1,15 @@
 $(document).ready(function() {
     
     $(window).bind('load', function() {
-         $(':animated').promise().done(function(){
-             doneResizing();
-         });
-    });
+        $(':animated').promise().done(function(){
             
-    var navigation = $('nav'),
-       items = navigation.find('.item');
-       //itemWidth = 150
-    
-    $(".item a").click(function(){
-        var item = $(this).parent(),
-           index = items.index(item),
-           leftPoint = 0;
-           
-        $(".item a").removeClass('active');
-        $(this).addClass('active');
-    
-        if (items.length-1 === index) {
-            leftPoint = navigation.width() - item.outerWidth(true);
-        } else if (index > 0) {
-            items.each(function(i, el) {
-                if (i >= index) {
-                    return;
-                }
-                leftPoint += $(el).outerWidth(true);
-            });
-        }
-    
-        $(".marker").stop().animate({left:leftPoint},200, function() {
-            $(".marker").stop().animate({display:'show'}, 200);
+            checkHash();
+             
+            setTimeout(function() {
+                doneResizing();
+            }, 1000);
         });
     });
-    
     
     $('.block').hover(function() {
         $(this).addClass('active');
@@ -50,58 +26,73 @@ $(document).ready(function() {
         $(this).parent().removeClass('active');
     });
     
-    $('a[data-filter="*"]').click(function(){
-        $('#navWrapper').css('background-color', '#980002');
-    });
-
-    $('a[data-filter=".webdev"]').click(function(){
-        $('#navWrapper').css('background-color', '#e91348');
-    });
     
-    $('a[data-filter=".scripts"]').click(function(){
-        $('#navWrapper').css('background-color', '#00c2f4');
-    });
     
-    $('a[data-filter=".photo"]').click(function(){
-        $('#navWrapper').css('background-color', '#8b49ab');
+    function checkHash(){
+        var hash = $(location).attr("hash").substring(1);
+        filterTiles(hash);
+    }
+    $(window).bind('hashchange', function (){
+       checkHash();
     });
-    
-    $('a[data-filter=".ps"]').click(function(){
-        $('#navWrapper').css('background-color', '#00cb66');
-    });
-    
-   
-   
    
     var $container = $('#blocksWrapper');
-   
-    // add randomish size classes
-    $container.find('.block').each(function(){
-        var $this = $(this),
-           number = parseInt( $this.find('.number').text(), 10 );
-        if ( number % 7 % 2 === 1 ) {
-         $this.addClass('width2');
-        }
-        if ( number % 3 === 0 ) {
-         $this.addClass('height2');
-        }
-    });
-        
+           
     $container.isotope({
         itemSelector : '.cont'
     });
-     
-    // filter items when filter link is clicked
-    $('nav a').click(function(){
-      var selector = $(this).attr('data-filter');
-      $container.isotope({ filter: selector });
-      $(':animated').promise().done(function(){
-          clearTimeout(this.id);
-          this.id = setTimeout(doneResizing, 1000);
-      });
-      return false;
-    });
-
+    
+    function filterTiles(selector) {
+        if (selector === 'all' || selector === '') {
+            $container.isotope({ filter: "*" });
+        }
+        else {
+            $container.isotope({ filter: (".").concat(selector) });
+        }
+        $(':animated').promise().done(function(){
+            clearTimeout(this.id);
+            this.id = setTimeout(doneResizing, 1000);
+        });
+        
+        $('#navWrapper a').removeClass('active');
+        $('#navWrapper a').each(function(){
+           if ($(this).attr('href') === ("#").concat(selector)){
+               $(this).addClass('active');
+               
+               //calculate position of the marker
+               var navigation = $('nav'),
+               items = navigation.find('.item');
+               var item = $(this).parent(),
+                  index = items.index(item),
+                  leftPoint = 0;
+               
+               if (items.length-1 === index) {
+                   leftPoint = navigation.width() - item.outerWidth(true);
+               } else if (index > 0) {
+                   items.each(function(i, el) {
+                       if (i >= index) {
+                           return;
+                       }
+                       leftPoint += $(el).outerWidth(true);
+                   });
+               }
+               
+               $(".marker").stop().animate({left:leftPoint},200, function() {
+                   $(".marker").stop().animate({display:'show'}, 200);
+               });
+               
+           }
+           else if (selector === '') {
+               window.console.log('bam');
+               $('#navWrapper li:nth-child(2) a').addClass('active');
+           }
+        });
+        
+        $('#navWrapper').removeClass();
+        $('#navWrapper').addClass(selector);
+        
+        return false;
+    }
     
     $(window).resize(function() {
         $(':animated').promise().done(function(){
@@ -123,12 +114,8 @@ $(document).ready(function() {
         var hDocument = $('body').height(); // get the height of your content
         var hWindow = $(window).height(); // get the height of the visitor's browser window
         var hFooter = $('footer').height();
-        
-        
-        
         var position = $('footer').css('position');
-        
-        //$('header').html("<div style=\"color: white;\">hDocument: "+hDocument+"<br>hWindow: "+hWindow+"<br>hFooter: "+hFooter+"<br>Position: "+position+"</div>");
+        //$('footer').css('bottom', '-120px');
         
         if (position === 'absolute'){
             if(hDocument>hWindow || hDocument+hFooter>hWindow) {
@@ -146,8 +133,6 @@ $(document).ready(function() {
                 $('footer').css('position', 'absolute');
             }
         }
-        
-        //$('header').html("<div style=\"color: white;\">hDocument: "+hDocument+"<br>hWindow: "+hWindow+"<br>hFooter: "+hFooter+"<br>Position: "+$('footer').css('position')+"</div>");
+        $('footer').css('bottom', '0');
     }
-   
 });
